db64296a1d15259e017e4b619cc93d1723e2c01b
laurenwilson27 <140430039+laurenwilson27@users.noreply.github.com>
2023-12-14 14:40:21 -0330

    Basic comment listing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header";
 import TopicList from "./components/TopicList";
 import ForumList from "./components/ForumList";
+import TopicView from "./components/TopicView";
 import Register from "./components/Register";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -17,7 +18,7 @@ function App() {
           <Routes>
             <Route path="/register" element={<Register />} />
             <Route path="/forum/:forumID" element={<TopicList />} />
-            <Route path="/topic/:topicID" />
+            <Route path="/topic/:topicID" element={<TopicView />} />
             <Route path="/" element={<ForumList />} />
           </Routes>
         </main>
--- src/components/TopicView.jsx
+++ src/components/TopicView.jsx
@@ -1,16 +1,16 @@
 import useGet from "../hooks/useGet";
 import { Link, useParams } from "react-router-dom";
 
-const TopicList = () => {
-  // The Router in App.js passes a forumID parameter based on the page URL
+const TopicView = () => {
+  // The Router in App.js passes a topicID parameter based on the page URL
   // The forumID parameter is used to apply a filter to the json-server request
   const params = useParams();
   const { data, isLoading, error } = useGet(
-    `http://localhost:7000/topics?forum=${params.forumID}`
+    `http://localhost:7000/comments?topic=${params.topicID}`
   );
 
   // Show placeholders if loading is in progress or has failed
-  if (isLoading) return <div>Loading topic listing...</div>;
+  if (isLoading) return <div>Loading comments...</div>;
   if (error)
     return (
       <div>
@@ -22,19 +22,18 @@ const TopicList = () => {
 
   return (
     <div>
-      <ul>
-        {data.map((topic) => {
+      Comments in this topic:
+      <div>
+        {data.map((comment) => {
           return (
-            <li>
-              <Link to={`/topic/${topic.id}`} key={topic.id}>
-                {topic.title}
-              </Link>
-            </li>
+            <div className="comment" key={comment.id}>
+              {comment.text}
+            </div>
           );
         })}
-      </ul>
+      </div>
     </div>
   );
 };
 
-export default TopicList;
+export default TopicView;
