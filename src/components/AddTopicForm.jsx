25b3381738720634c948ff0dd13331a9243b876d
laurenwilson27 <140430039+laurenwilson27@users.noreply.github.com>
2023-12-15 21:18:14 -0330

    Form for users to post new topics, topic count is tracked

---
+++ src/components/AddTopicForm.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+const AddTopicForm = ({ onAdd }) => {
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!text) {
+      alert("Please add a description of your topic before submitting.");
+      return;
+    }
+
+    if (!title) {
+      alert("Please add a topic title before submitting.");
+      return;
+    }
+
+    onAdd(title, text);
+
+    setText("");
+    setTitle("");
+  };
+
+  return (
+    <form className="add-comment" onSubmit={onSubmit}>
+      <label htmlFor="title">Title:</label>
+      <input
+        id="title"
+        type="text"
+        placeholder="Enter topic title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <label>Comment:</label>
+      <textarea
+        type="text"
+        placeholder="Enter topic description"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      ></textarea>
+      <input type="submit" value="Create Topic"></input>
+    </form>
+  );
+};
+
+export default AddTopicForm;
