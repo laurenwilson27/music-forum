6a799fb6cd6c0f3e9f6216cad27acb7ed60243fb
jnchiarot <140640440+jnchiarot@users.noreply.github.com>
2023-12-20 13:33:46 -0330

    Register page working.

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,59 +1,74 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from "react";
 
 function Register() {
+  const [users, setUsers] = useState([]);
+  // var [users, setUsers] = useState(users.id);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   useEffect(() => {
-    const getUser = async() => {
+    const getUser = async () => {
       const userFromServer = await fetchUser();
-      setUser(userFromServer);
+      setUsers(userFromServer);
     };
     getUser();
   }, []);
 
-    const fetchUser = async(id) => {
-      const res = await fetch(`http://localhost:5000/user/${id}`);
-      const data = await res.json();
-      return data;
-    }
+  const fetchUser = async (id) => {
+    const res = await fetch(`http://localhost:7000/users`);
+    const data = await res.json();
+    return data;
+  };
 
-  setUser(
-    users.map((user) => 
-    user.id === id ? {...user, reminder: !user.reminder} : user
-    )
-  )
-};
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
 
-const addUser = async (user) => {
-  const res = await fetch("http://localhost:5000/user", 
-    {method: 'POST', headers:{"Content-type":"application/json"},
-    body: JSON.stringify(user),
-  });
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
 
-  const data = await res.json();
-  setUsers([...user, data]);
-}
+  const addUser = async () => {
+    const newUser = { username, password };
 
-return (
-  <div className="loginbox">
+    const res = await fetch("http://localhost:7000/users", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(newUser),
+    });
+
+    const data = await res.json();
+    setUsers((prevUsers) => [...prevUsers, data]);
+  };
+
+  return (
+    <div className="loginbox">
       <h2>Register</h2>
       <div className="userbox">
-          <label htmlFor="username">Username:</label>
-          <input type="text"
-              id="username" 
-              value={username} 
-              onChange={handleUsernameChange} 
-              placeholder="Enter your username" />
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={handleUsernameChange}
+          placeholder="Enter your username"
+        />
       </div>
       <div className="passbox">
-          <label htmlFor="password">Password:</label>
-          <input type="password" 
-              id="password" 
-              value={password} 
-              onChange={handlePasswordChange} 
-              placeholder="Enter your password" />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+          placeholder="Enter your password"
+        />
       </div>
-      <button class="btn" onclick={addUser}>Log In</button>
-  </div>
-);
-
+      <button className="btn" onClick={addUser}>
+        Log In
+      </button>
+    </div>
+  );
+}
 
 export default Register;
